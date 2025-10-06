b12462c75fb62be968441b89fa8c1ba2d2241ba9
YauhenEp <74007728+YauhenEp@users.noreply.github.com>
2025-10-06 20:00:54 +0200

    Update 123.js

--- lesson-02/123.js
+++ lesson-02/123.js
@@ -5,3 +5,7 @@ function add(numberOne, numberTwo) {
 function minus(numberOne, numberTwo) {
   return numberOne - numberTwo;
 }
+
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
